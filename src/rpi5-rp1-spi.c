56663035d5e1982ac995de0b2e71edff5d89291f
Paweł Szymański <pawelfiu@gmail.com>
2024-06-11 11:19:12 +0200

    trimmin'

--- src/rpi5-rp1-spi.c
+++ src/rpi5-rp1-spi.c
@@ -38,38 +38,6 @@ void delay_ms(int milliseconds)
     nanosleep(&ts, NULL);
 }
 
-// pci bar info
-// from: https://github.com/G33KatWork/RP1-Reverse-Engineering/blob/master/pcie/hacks.py
-#define RP1_BAR1 0x1f00000000
-#define RP1_BAR1_LEN 0x400000
-
-// offsets from include/dt-bindings/mfd/rp1.h
-// https://github.com/raspberrypi/linux/blob/rpi-6.1.y/include/dt-bindings/mfd/rp1.h
-#define RP1_IO_BANK0_BASE 0x0d0000
-#define RP1_RIO0_BASE 0x0e0000
-#define RP1_PADS_BANK0_BASE 0x0f0000
-
-// the following info is from the RP1 datasheet (draft & incomplete as of 2024-02-18)
-// https://datasheets.raspberrypi.com/rp1/rp1-peripherals.pdf
-#define RP1_ATOM_XOR_OFFSET 0x1000
-#define RP1_ATOM_SET_OFFSET 0x2000
-#define RP1_ATOM_CLR_OFFSET 0x3000
-
-#define PADS_BANK0_VOLTAGE_SELECT_OFFSET 0
-#define PADS_BANK0_GPIO_OFFSET 0x4
-
-#define RIO_OUT_OFFSET 0x00
-#define RIO_OE_OFFSET 0x04
-#define RIO_NOSYNC_IN_OFFSET 0x08
-#define RIO_SYNC_IN_OFFSET 0x0C
-//                           3         2         1
-//                          10987654321098765432109876543210
-#define CTRL_MASK_FUNCSEL 0b00000000000000000000000000011111
-#define PADS_MASK_OUTPUT  0b00000000000000000000000011000000
-
-#define CTRL_FUNCSEL_RIO 0x05
-
-
 void *mapgpio(off_t dev_base, off_t dev_size)
 {
     int fd;
