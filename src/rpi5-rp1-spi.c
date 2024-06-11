df9dd44431ee893aab1d7c959141d98bb34978b6
Paweł Szymański <pawelfiu@gmail.com>
2024-06-11 12:49:38 +0200

    testin'

--- src/rpi5-rp1-spi.c
+++ src/rpi5-rp1-spi.c
@@ -169,7 +169,7 @@ int main(void)
     *(volatile uint32_t *)(spi->regbase + DW_SPI_SSIENR) = 0x1;
 
     // LED data
-    int leds_count = 10;
+    int leds_count = 30;
     int data_length = leds_count * 3;
     uint8_t data[data_length];
 
